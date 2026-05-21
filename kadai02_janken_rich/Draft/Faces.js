df266f75c79b985ad8eb36dfe73457f3a7401e31
midoriino <midori.kuro.614@gmail.com>
2026-05-22 03:04:30 +0900

    Week3

---
+++ kadai02_janken_rich/Draft/Faces.js
@@ -0,0 +1,125 @@
+
+const video = document.getElementById('video')
+const startBtn = document.getElementById('startvideobtn')
+let stream = null;
+let FaceMatcher = null
+
+startBtn.addEventListener('click', toggleCamera) /*★Event listeners*/
+
+function toggleCamera (){
+    if (stream === null) {
+        startCamera()
+    } else {
+        stopCamera()
+    }
+}
+
+async function startCamera() {
+    stream = await navigator.mediaDevices.getUserMedia( {video : true})
+    video.srcObject = stream
+    startBtn.innerHTML = "Stop Camera"
+    detectFace()
+}
+
+async function stopCamera () {
+    stream.getTracks().forEach(track => track.stop()) 
+    /*
+    "Get all the tracks in the stream, and for each one, stop it.""
+    getTracks()
+    forEach 
+    ★ track => track.stop means the same as 
+    function stop(track) {
+        track.stop()
+    }*/
+    video.srcObject = null
+    startBtn.innerHTML = "Start Camera"
+}
+
+async function loadModels(){//please download the AI weight files from faceapi into memory  
+    await faceapi.nets.ssdMobilenetv1.loadFromUri('./models')
+    await faceapi.nets.faceLandmark68Net.loadFromUri('./models')
+    await faceapi.nets.faceRecognitionNet.loadFromUri('./models')
+    console.log('models loaded')
+}
+
+loadModels().then(() => loadKnownFaces())
+
+async function detectFace(){ 
+    // Define an async function called detectFace. 
+    console.log('detectFace running')
+    // Log a debug message. 
+    setInterval(async () => { // the async before the => means that the  
+        if (video.readyState === 4){
+    // use of await within the "arrow" function is enabled?? 
+        const detections = await faceapi.detectAllFaces(video, new faceapi.SsdMobilenetv1Options({ minConfidenceScore: 0.3 }))//Looks at the frame in the video
+        // take the current frame from the video element, 
+        // run Face API's face detection on it, and log whatever it finds
+        console.log('detections: ',detections)
+
+               if (detections.length > 0) {
+                    const liveDetection = await faceapi.detectSingleFace(video)
+                        .withFaceLandmarks()
+                        .withFaceDescriptor()
+                    
+                    if (liveDetection) {
+                        const match = faceMatcher.findBestMatch(liveDetection.descriptor)
+                        // console.log('match:', match.label)
+                        if(match.label === 'Midori') {
+                            document.getElementById('scan-message').innerHTML = 'Hello Midori!'
+                            document.querySelector('.scanner').classList.add('authenticated')
+                        } else {
+                            document.getElementById('scan-message').innerHTML = 'Face not recognized. Please register yourself.'
+                            document.querySelector('.scanner').classList.add('unknown')
+                            console.log(document.querySelector('#scanner').classList)
+                        }
+                        setTimeout(() => {
+                            document.getElementById('scan-message').innerHTML = 'Scanning...'
+                            document.querySelector('.scanner').classList.remove
+                            document.querySelector('.scanner').classList.add('base')
+                            document.documentElement.style.setProperty('--corner-color', 'darkcyan')
+                        },6000)
+                    }
+                }
+
+        }
+        //faceapi: the app, used to detect the faces from "video" stream
+    }, 300)
+    // every 300 milliseconds, 
+}
+
+
+async function loadKnownFaces() {
+    const image = await faceapi.fetchImage('./known_faces/Midori.jpg')
+    const detection = await faceapi.detectSingleFace(image)
+        .withFaceLandmarks()
+        .withFaceDescriptor()
+    console.log('descriptor', detection.descriptor)
+    const labelledDescriptor = new faceapi.LabeledFaceDescriptors(
+        'Midori',
+        [detection.descriptor]
+    )
+    faceMatcher = new faceapi.FaceMatcher(labelledDescriptor)
+    console.log('faceMatcher ready: ', faceMatcher)
+}
+
+
+// _score — confidence that it's a face (0 to 1)
+// _box — the coordinates of where the face is in the frame (x, y, width, height)
+// _imageDims — the dimensions of the video frame
+
+// async function startCamera() {
+//     const stream = await navigator.mediaDevices.getUserMedia( {video : true} )
+// }
+
+// setInterval(async () => {
+//     //scan for faces here 
+
+// }, 3000) //every 3000 milliseconds
+
+// if (match found){
+//     //show Hello name
+// } if else {
+//     //show registration prompt
+// }
+
+
